b96db9d9bd6e0ccd769fce937808304a8bf78054
aaryanvekariya <aaryanvekariya0143@gmail.com>
2025-02-11 13:47:21 +0530

    Add By Task Fomatting

---
+++ Task2/src/App.jsx
@@ -0,0 +1,66 @@
+// const app = () =>{
+//   return (
+//     <>
+//     <h1 className="Headache">Aryan</h1>
+//     <h2>Jesh Paghadal</h2>
+//     <img src="vite.svg" alt="Rnder image"/>
+//     </>
+//   )
+// }
+
+// export default app;
+
+
+import Greetings from "./components/Greetings";
+ 
+
+const HeaderApp = () => {
+  return (
+    <div>
+      
+      <Greetings/>
+      
+    </div>
+  )
+}
+
+export default HeaderApp;
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
---
+++ Task3/src/components/Greetings.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Greetings = () => {
+  return (
+    <>
+    Hello Buddy
+    </>  )
+}
+
+export default Greetings
