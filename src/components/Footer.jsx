8b0ac41933245affc90cc532b81474d27051df2e
aaryanvekariya <aaryanvekariya0143@gmail.com>
2025-02-11 11:35:03 +0530

    Add files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+    <p>© 2025 Aryan Vekariya</p>
+   </footer>
+  )
+}
+
+export default footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+  <h1>Welcome to My Portfolio</h1>
+  <nav className="navbar">
+    <a href="#Home">Home</a>
+    <a href="#about">About</a>
+    <a href="#contact">Contact</a>
+  </nav>
+</header>
+  )
+}
+
+export default Header
